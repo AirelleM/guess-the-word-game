c5b8dbc9b0b2fba33ef9d597f131219d3420b786
Airelle M <Airelle.code@gmail.com>
2025-04-28 14:59:24 -0600

    Validate player guesses and track guessed letters

--- js/script.js
+++ js/script.js
@@ -6,7 +6,20 @@ const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+const guessedLetters = [];
 
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter only one letter at a time.";
+    } else if (!input.match (acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+};
 
 let word = "magnolia"; 
 
@@ -24,7 +37,27 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+
+    message.innerText = "";
+
     const guess = letterInput.value;
-    console.log (guess);
+    const goodGuess = validateInput(guess);
+    if (goodGuess) {
+        makeGuess (goodGuess);
+    }
+
+
     letterInput.value = "";
 });
+
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+
+if (guessedLetters.includes (guess)) {
+    message.innerText = "You already guessed that letter. Try again!";
+} else {
+    guessedLetters.push (guess);
+    console.log (guessedLetters);
+}
+};
